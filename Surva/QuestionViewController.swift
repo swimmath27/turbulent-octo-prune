451e4285813fe4c13d0e21a22cd047c3f9f9b801
Michael Lombardo <swimmath27@comcast.net>
2015-09-13 03:52:43 -0400

    got first question image to display for selected category

--- Surva/QuestionViewController.swift
+++ Surva/QuestionViewController.swift
@@ -12,16 +12,53 @@ import Parse
 class QuestionViewController: UIViewController {
     
     var currentObject: PFObject?
-    
-    override func viewWillAppear(animated: Bool) {
-        label.text = currentObject?.objectId
-    }
 
-    @IBOutlet weak var label: UILabel!
-    
+    @IBOutlet weak var IV: UIImageView!
+
+    var questions: [PFObject] = [PFObject]()
+
     override func viewDidLoad() {
         super.viewDidLoad()
-        //label.text = categoryID
+
+        var query = PFQuery(className: "Question")
+        query.whereKey("CategoryID", equalTo: currentObject!.objectId!)
+
+
+        query.findObjectsInBackgroundWithBlock {
+            (objects: [AnyObject]?, error: NSError?) -> Void in
+
+            if error == nil {
+                // The find succeeded.
+
+
+                // put found objects into array
+                if let objects = objects as? [PFObject] {
+                    for object in objects {
+                        self.questions.append(object)
+                    }
+                }
+                if (self.questions.count > 0)
+                {
+                    //display first object
+                    let pic = self.questions[0]["Picture"] as! PFFile
+
+                    pic.getDataInBackgroundWithBlock { (imageData: NSData?, error:      NSError?) -> Void in
+                        if (error == nil) {
+                            self.IV.image = UIImage(data:imageData!)
+                        }
+                    }
+                }
+                else
+                {
+
+                }
+            } else {
+                // Log details of the failure
+                println("Error: \(error!) \(error!.userInfo!)")
+            }
+        }
+
+
         // Do any additional setup after loading the view.
     }
 
